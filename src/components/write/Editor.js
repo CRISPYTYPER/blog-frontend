f9bb439092c11dce0ce3d05663f87d7c99c4f3ec
CRISPYTYPER <jack3409@naver.com>
2022-05-26 01:14:08 +0900

    page 825 fix typo

--- src/components/write/Editor.js
+++ src/components/write/Editor.js
@@ -33,7 +33,7 @@ const QuillWrapper = styled.div`
     }
 `;
 
-const Editor = ([title, body, onChangeField]) => {
+const Editor = ({ title, body, onChangeField }) => {
     const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
     const quillInstance = useRef(null); // Quill 인스턴스를 설정
 
